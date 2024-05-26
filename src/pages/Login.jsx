a31fc59ea49032eaf5a57ec95e894bbc7ffdf1fc
Chepe <jose.ramirez93u@std.uni.edu.ni>
2024-05-25 20:15:24 -0600

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,24 @@
+function Login() {
+  return (
+    <div className="flex h-dvh w- justify-center items-center">
+      <LoginContainer></LoginContainer>
+    </div>
+  );
+}
+
+function LoginContainer() {
+  return (
+    <div className="flex w-5/6 sm:w-2/3 h-5/6 sm:h-2/3 bg-pharmaticFade p-3 rounded-xl">
+      <PillsBackground />
+      <div className="w-full md:w-1/2 h-full bg-orange-100 rounded-r-xl"></div>
+    </div>
+  );
+}
+
+function PillsBackground() {
+  return (
+    <div className="w-1/2 h-full hidden md:block bg-pills-background bg-cover rounded-l-xl"></div>
+  );
+}
+
+export default Login;
