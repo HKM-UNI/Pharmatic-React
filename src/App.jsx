b5842f6d8a5150488944d7c10b667971c62182a6
darkzense <torresjohn094@gmail.com>
2024-05-29 12:21:07 -0600

    fetch hooks + customers example

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import init_axios_defaults from "@/lib/axios_defaults";
+import axios from "axios";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SWRConfig } from "swr";
 import Login from "./pages/Login";
 import MainLayout from "./shared/MainLayout";
 
+init_axios_defaults();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="*" element={<MainLayout />} />
-      </Routes>
-    </BrowserRouter>
+    <SWRConfig
+      value={{
+        fetcher: (url) => axios.get(url).then((res) => res.data),
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<MainLayout />} />
+        </Routes>
+      </BrowserRouter>
+    </SWRConfig>
   );
 }
 
---
+++ src/data/customer.js
@@ -0,0 +1,9 @@
+export class Customer {
+  birthDate = ""
+  customerNo = 0
+  email = ""
+  gender = ""
+  name = ""
+  phone = 0
+  surname = ""
+}
