7aefe3781b9b623fb034aeb0682014cbc3cc9931
darkzense <torresjohn094@gmail.com>
2024-06-08 22:53:03 -0600

    match sales page style to product page style

--- src/pages/SalesHistory.jsx
+++ src/pages/SalesHistory.jsx
@@ -5,14 +5,14 @@ import {
   CalendarDays,
   ShoppingCart,
   UserRound,
-  UserRoundX
+  UserRoundX,
 } from "lucide-react";
 import Error from "./Error";
 import LoadingPanel from "./LoadingPanel";
 
 function SalesCard({ children }) {
   return (
-    <div className="2xl:text-md flex flex-col rounded-lg bg-gray-100 p-3 text-sm shadow-md 2xl:p-5">
+    <div className="2xl:text-md flex flex-col rounded-lg bg-gray-100 p-5 text-sm shadow-md transition delay-150 ease-in-out hover:-translate-y-1 hover:scale-102 hover:drop-shadow-md">
       {children}
     </div>
   );
@@ -31,10 +31,10 @@ export default function SalesHistory() {
 
   return (
     <DynamicPanel>
-      <div className="pharmatic-card-grid">
+      <div className="grid h-full grid-cols-1 gap-6 p-6 md:grid-cols-2 2xl:grid-cols-3">
         {sales.map((s) => (
           <SalesCard key={s.invoiceNo}>
-            <div className="space-y-5">
+            <div className="space-y-3">
               <div className="flex items-center justify-between text-lg font-semibold">
                 <div className="inline-flex items-center gap-2 text-teal-600">
                   <Banknote />
@@ -45,14 +45,16 @@ export default function SalesHistory() {
                   <ShoppingCart />
                 </div>
               </div>
-              <div className="h-[15em] overflow-auto whitespace-nowrap rounded bg-white p-4">
+              <div className="h-[12rem] overflow-auto whitespace-nowrap rounded bg-white p-4">
                 {s.products.map((p, idx) => (
-                  <p key={idx} className="mb-3 font-normal">{p}</p>
+                  <p key={idx} className="mb-3 font-normal">
+                    {p}
+                  </p>
                 ))}
               </div>
               <div className="inline-flex items-center gap-2">
                 <CalendarDays />
-                <p className="font-semibold">
+                <span className="font-semibold">
                   {s.salesDate.toLocaleDateString("es-ES", {
                     year: "numeric",
                     month: "2-digit",
@@ -64,7 +66,7 @@ export default function SalesHistory() {
                     minute: "numeric",
                     hour12: true,
                   })}
-                </p>
+                </span>
               </div>
               <div className="flex items-center justify-end gap-2">
                 <p className="font-normal">{s.customerName}</p>
