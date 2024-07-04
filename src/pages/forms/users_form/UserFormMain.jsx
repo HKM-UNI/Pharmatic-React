e8d8618de4d2ff94cf4dc18a8aa67aaf9693705c
darkzense <torresjohn094@gmail.com>
2024-07-04 13:00:06 -0600

    responsive user form

--- src/pages/forms/users_form/UserFormMain.jsx
+++ src/pages/forms/users_form/UserFormMain.jsx
@@ -4,8 +4,8 @@ import UserFormImage from "./UserFormImage";
 /** @param {{ edit: boolean, defaultImgSrc: string }} */
 export default function UserFormMain({ edit = false, defaultImgSrc }) {
   return (
-    <div className="flex items-start justify-center gap-10 pb-5 pt-5 2xl:pt-16">
-      <div className="w-80 space-y-4 md:w-2/5 2xl:w-1/4">
+    <div className="flex flex-wrap md:flex-nowrap items-start justify-center gap-10 pb-5 pt-5 2xl:pt-16">
+      <div className="w-90 space-y-4 md:w-2/5 2xl:w-1/4">
         <div className="grid grid-cols-2 gap-x-6 gap-y-4 2xl:grid-cols-1">
           <FormInput
             fieldname="username"
